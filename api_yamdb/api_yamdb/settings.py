4a58248626a849ccd998c99376b5181140512fc7
Victor_Gostyaikin <gostyaikin@yandex.ru>
2022-01-22 18:35:59 +0300

    staticfiles

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'reviews/static/'),
+)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
